726a5e3d8cc1891acf17b2014b2ffe7d87b542cc
webcontive <wc.bhadresh@gmail.com>
2020-01-17 09:42:40 +0530

    change in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	resources :sessions, only: [:new, :create, :destroy]
+	post "/deactivate", to: "principles#deactivate"
 	get 'signup', to: 'principles#new', as: 'signup'
 	get 'login', to: 'sessions#new', as: 'login'
 	get 'logout', to: 'sessions#destroy', as: 'logout'
